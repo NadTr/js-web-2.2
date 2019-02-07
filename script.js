9f8143568abb272eaa884c046ee599b75fe933a3
NadTr <trauflernadine@gmail.com>
2019-02-07 15:50:48 +0100

    version2

--- script.js
+++ script.js
@@ -17,39 +17,32 @@ import axios from 'axios';
 var inside = document.querySelector(".inside");
 //fonction asynchrone car sinon, il ne recharge pas l'API
 async function getoneBeer() {
-        await axios.get('https://api.punkapi.com/v2/beers/random') //then attend que await soit fini
-        .then(function (response) {
+         let response = await axios.get('https://api.punkapi.com/v2/beers/random') //then attend que await soit fini
+
           var beer= new Array();
-
-            beer[0] = "bonjour"//response.data.name;
-            beer[1] = response.data.tagline;
-            beer[2] = response.data.first_brewed;
-            beer[3] = response.data.image_url;
-            return beer;
-        })
-        .catch(function (error) {
-            error.innerHTML = "(An error has occurred.)";
-        });
+console.log(response.data[0]);
+            beer[0] =response.data[0].name;
+            beer[1] = response.data[0].tagline;
+            beer[2] = response.data[0].first_brewed;
+            beer[3] = response.data[0].image_url;
+            let li = document.createElement("li");
+            li.className="beerCase"
+            let h2 = document.createElement("h2");
+            let h3 = document.createElement("h3");
+            let p = document.createElement("p");
+            let img= new Image();
+            let beerName= document.createTextNode(beer[0]);
+            let beerTag= document.createTextNode(beer[1]);
+            let beerBrew= document.createTextNode(beer[2]);
+            img.src=beer[3];
+            h2.appendChild(beerName);
+            h3.appendChild(beerTag);
+            p.appendChild(beerBrew);
+            li.appendChild(h2);
+            li.appendChild(h3);
+            li.appendChild(p);
+            li.appendChild(img);
+            inside.appendChild(li);
+            getoneBeer();
     }
-
-
-  for (let i = 0; i < 16; i++){
-		let li = document.createElement("li");
-    li.className="beerCase"
-    let h2 = document.createElement("h2");
-    let h3 = document.createElement("h3");
-		let p = document.createElement("p");
-		let oneBeer = getoneBeer();
-    let beerName= document.createTextNode("bière");//oneBeer[0]);
-    let beerTag= document.createTextNode(JSON.stringify(oneBeer[1]));
-    let beerBrew= document.createTextNode(JSON.stringify(oneBeer[2]));
-    //let beerImf= document.createTextNode(oneBeer[0]);
-
-    h2.appendChild(beerName);
-    h3.appendChild(beerTag);
-		p.appendChild(beerBrew);
-    li.appendChild(h2);
-    li.appendChild(h3);
-		li.appendChild(p);
-		inside.appendChild(li);
-	}
+getoneBeer();
