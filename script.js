90cbc6d4f2effc4fd4ed88b532f9d245d73cc45b
NadTr <trauflernadine@gmail.com>
2019-02-08 14:40:00 +0100

    finish

--- script.js
+++ script.js
@@ -17,38 +17,36 @@ import axios from 'axios';
 var inside = document.querySelector(".inside");
 //fonction asynchrone car sinon, il ne recharge pas l'API
 async function getallBeers() {
-         let response = await axios.get('https://api.punkapi.com/v2/beers/') //then attend que await soit fini
-
-         try{
-           for (var i=0;i<response.data.length ;i++){
-             var bName =response.data[i].name;
-             var bTag = response.data[i].tagline;
-             var bYear = response.data[i].first_brewed;
-             var bImg = response.data[i].image_url;
-             let li = document.createElement("li");
-             li.className="beerCase"
-             let h2 = document.createElement("h2");
-             let h3 = document.createElement("h3");
-             let p = document.createElement("p");
-             let img= new Image();
-             let beerName= document.createTextNode(bName);
-             let beerTag= document.createTextNode(bTag);
-             let beerBrew= document.createTextNode(bYear);
-             img.src=bImg;
-             h2.appendChild(beerName);
-             h3.appendChild(beerTag);
-             p.appendChild(beerBrew);
-             li.appendChild(h2);
-             li.appendChild(h3);
-             li.appendChild(p);
-             li.appendChild(img);
-             inside.appendChild(li);
-           }
-        }
-        catch(error){
-          console.error(error);
-        }
+   try{
+     let response = await axios.get('https://api.punkapi.com/v2/beers/') //then attend que await soit fini
+     for (var i=0;i<response.data.length ;i++){
+       var bName =response.data[i].name;
+       var bTag = response.data[i].tagline;
+       var bYear = response.data[i].first_brewed;
+       var bImg = response.data[i].image_url;
+       let li = document.createElement("li");
+       li.className="beerCase"
+       let h2 = document.createElement("h2");
+       let h3 = document.createElement("h3");
+       let p = document.createElement("p");
+       let img= new Image();
+       let beerName= document.createTextNode(bName);
+       let beerTag= document.createTextNode(bTag);
+       let beerBrew= document.createTextNode(bYear);
+       img.src=bImg;
+       h2.appendChild(beerName);
+       h3.appendChild(beerTag);
+       p.appendChild(beerBrew);
+       li.appendChild(h2);
+       li.appendChild(h3);
+       li.appendChild(p);
+       li.appendChild(img);
+       inside.appendChild(li);
+      }
     }
-
+    catch(error){
+      console.error(error);
+    }
+  }
 
 getallBeers();
